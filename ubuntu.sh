797f648bf9676ac7cf1f4de8de85cb93491ddea1
Shri Ganesh Jha <realsgj@gmail.com>
2022-05-25 21:38:55 +0530

    Example IP address

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,10 +1,10 @@
 # function to ssh into ubuntu vm
 
 function ubuntu_vm () {
-	echo "Welcome to glinux, Sir"
+	echo "Welcome to your cloud vm!"
 	echo -en "\007"
 	bash hello.sh
-	paplay notify.wav && ssh sgj@34.131.139.84
+	paplay notify.wav && ssh sgj@<your_ip_address>
 }
 
 ubuntu_vm
